725b82a62b605b745288fc678089132c2b1198da
Samuel Karanja <samuel.karanja@student.moringaschool.com>
2023-03-17 14:50:42 +0300

    completed assginment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+function writeCards(names, occasion) {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${occasion} gift!`);
+  }
+  
+  function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
